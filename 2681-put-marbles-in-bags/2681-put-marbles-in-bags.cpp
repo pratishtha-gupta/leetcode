283e5a33aeb213c38175fff82ef21304a816ece3
Pratishtha Gupta <166021416+pratishtha-gupta@users.noreply.github.com>
2025-03-31 12:37:37 +0530

    Time: 39 ms (43.88%), Space: 67.4 MB (63.29%) - LeetHub

---
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        vector <int> pairsum;
+        if(k==1) return 0;
+        for(int i=0;i<weights.size()-1;i++)
+        {
+            long long sum = weights[i]+weights[i+1];
+            pairsum.push_back(sum);
+        }
+        sort(pairsum.begin(),pairsum.end());
+        long long minsum=0,maxsum=0;
+        for(int i=0;i<k-1;i++)
+        {
+            minsum+=pairsum[i];
+            maxsum+=pairsum[pairsum.size()-(i+1)];
+        }
+        return maxsum-minsum;
+    }
+};
