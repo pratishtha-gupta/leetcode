7b02eb818ece9c20bd24fd8921a6d0636dc51c9c
Pratishtha Gupta <166021416+pratishtha-gupta@users.noreply.github.com>
2025-06-17 13:45:29 +0530

    Time: 83 ms (29.5%), Space: 53.9 MB (8.88%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int k) {
-        int n = arr.size(); // size of the given array.
-    map <int,int>mpp;
-    int preSum = 0, cnt = 0;
-
-    mpp[0] = 1; // Setting 0 in the map.
-    for (int i = 0; i < n; i++) {
-        // add current element to prefix Sum:
-        preSum += arr[i];
-
-        // Calculate x-k:
-        int remove = preSum - k;
-
-        // Add the number of subarrays to be removed:
-        cnt += mpp[remove];
-
-        // Update the count of prefix sum
-        // in the map.
-        mpp[preSum] += 1;
-    }
-    return cnt;
+    int subarraySum(vector<int>& nums, int k) {
+        int cnt = 0, presum = 0;
+        map<int, int> mp;
+        mp[0] = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            presum += nums[i];
+            int diff = presum-k;
+            cnt+= mp[diff];
+            mp[presum]+=1;
+        }
+        return cnt;
     }
 };
