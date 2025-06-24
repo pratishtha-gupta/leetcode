60aea16ebc7bdd59c7c0ed6a91168f210df486ef
Pratishtha Gupta <166021416+pratishtha-gupta@users.noreply.github.com>
2025-06-24 18:08:50 +0530

    Time: 0 ms (100%), Space: 8.6 MB (79.69%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,26 +1,23 @@
 class Solution {
-public:
-    void generate(int ind,int target,int k,int n,vector<int> &ds,vector<vector<int>> &ans)
-    {
-        if(ds.size()==k&&target==n)
-        {
-            ans.push_back(ds);
+    void generate(vector<vector<int>> &ans, int k, int n, vector<int> &ds, int sum, int ind) {
+        if (ds.size() == k) {
+            if (sum == n) ans.push_back(ds);
             return;
         }
-        for(int i=ind;i<=9;i++)
-        {
-            if(target>n) break;
-            //pick
-            ds.push_back(i);
-            generate(i+1,target+i,k,n,ds,ans);
-            ds.pop_back();
+        
+        for (int i = ind; i <= 9; i++) {
+            if (sum + i > n) break; 
+            ds.push_back(i);         
+            generate(ans, k, n, ds, sum + i, i + 1); 
+            ds.pop_back();             
         }
     }
+
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
         vector<int> ds;
-        generate(1,0,k,n,ds,ans);
+        generate(ans, k, n, ds, 0, 1); 
         return ans;
     }
 };
