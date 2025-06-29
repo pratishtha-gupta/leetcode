24060acea1a56fab49d6b204b9f69e1f542d4cf2
Pratishtha Gupta <166021416+pratishtha-gupta@users.noreply.github.com>
2025-06-29 16:05:54 +0530

    Time: 0 ms (100%), Space: 18 MB (77.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
-  
-    void BFS(vector<vector<int>>& image, vector<vector<int>>& visited, int sr, int sc, int color) {
-        int m = image.size();
-        int n = image[0].size();
-        visited[sr][sc] = 1;
-        int initial = image[sr][sc];
-        image[sr][sc] = color;
+    void dfs(vector<vector<int>>& image, int i, int j, int color, int oldColor) {
+        int m = image.size(), n = image[0].size();
+        if (i < 0 || j < 0 || i >= m || j >= n || image[i][j] != oldColor) return;
 
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
-        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        image[i][j] = color;
 
-        while (!q.empty()) {
-            auto node = q.front();
-            q.pop();
-            int first = node.first;
-            int second = node.second;
-
-            for (auto dir : directions) {
-                int newRow = first + dir.first;
-                int newCol = second + dir.second;
-
-                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n &&
-                    image[newRow][newCol] == initial && visited[newRow][newCol] == 0) {
-                    
-                    q.push({newRow, newCol});
-                    image[newRow][newCol] = color;
-                    visited[newRow][newCol] = 1;
-                }
-            }
-        }
+        dfs(image, i + 1, j, color, oldColor);
+        dfs(image, i - 1, j, color, oldColor);
+        dfs(image, i, j + 1, color, oldColor);
+        dfs(image, i, j - 1, color, oldColor);
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m = image.size();
-        int n = image[0].size();
-        vector<vector<int>> visited(m, vector<int>(n, 0));
-
-        if (image[sr][sc] != color) 
-            BFS(image, visited, sr, sc, color);
-
+        int oldColor = image[sr][sc];
+        if (oldColor != color)
+            dfs(image, sr, sc, color, oldColor);
         return image;
     }
 };
