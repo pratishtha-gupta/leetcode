4414975a6faad53164f112c734385fceee38d70c
Pratishtha Gupta <166021416+pratishtha-gupta@users.noreply.github.com>
2025-06-30 04:22:31 +0530

    Time: 1314 ms (78.02%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT name 
+FROM SalesPerson 
+WHERE sales_id NOT IN (
+    SELECT sales_id 
+    FROM Orders 
+    WHERE com_id IN (
+        SELECT com_id 
+        FROM Company 
+        WHERE name = 'RED'
+    ) 
+    AND sales_id IS NOT NULL
+) 
