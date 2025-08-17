2ad85e0c7b020c6845d636a0a273d9b8db8c032e
Pratishtha Gupta <166021416+pratishtha-gupta@users.noreply.github.com>
2025-08-17 23:18:17 +0530

    Time: 48 ms (12.36%), Space: 47.6 MB (31.16%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-    map<char,int> mp;
-    for (char t : tasks) {
-        mp[t]++;
+        // Building frequency map
+        int freq[26] = {0};
+        for (char &ch : tasks) {
+            freq[ch - 'A']++;
+        }
+
+        // Max heap to store frequencies
+        priority_queue<int> pq;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                pq.push(freq[i]);
+            }
+        }
+
+        int time = 0;
+        // Process tasks until the heap is empty
+        while (!pq.empty()) {
+            int cycle = n + 1;
+            vector<int> store;
+            int taskCount = 0;
+            // Execute tasks in each cycle
+            while (cycle-- && !pq.empty()) {
+                if (pq.top() > 1) {
+                    store.push_back(pq.top() - 1);
+                }
+                pq.pop();
+                taskCount++;
+            }
+            // Restore updated frequencies to the heap
+            for (int &x : store) {
+                pq.push(x);
+            }
+            // Add time for the completed cycle
+            time += (pq.empty() ? taskCount : n + 1);
+        }
+        return time;
     }
-
-    int maxi = 0;
-    for (auto &p : mp) {
-        maxi = max(maxi, p.second);  
-    }
-
-    int count_max = 0;
-    for (auto &p : mp) {
-        if (p.second == maxi) count_max++; 
-    }
-
-    int min_time = (maxi - 1) * (n + 1) + count_max;
-
-    return max((int)tasks.size(), min_time);
-}
-
 };
