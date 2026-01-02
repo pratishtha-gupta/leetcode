d0b65aa76882e56b2568cab80accd18bb47604d1
Pratishtha Gupta <166021416+pratishtha-gupta@users.noreply.github.com>
2026-01-02 15:46:11 +0530

    Time: 0 ms (100%), Space: 28.5 MB (53.6%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& A) {
+         for (int k = 1; k <= 3; ++k)
+            for (int i = 0; i < A.size() - k; ++i)
+                if (A[i] == A[i+k])
+                    return A[i];
+
+                    return -1;
+    }
+};
